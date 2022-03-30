0ff1853831876c32449cabab47e0b97a58ca5bf5
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-03-30 21:25:23 +0545

    Shopping Cart Basics

---
+++ src/store/cart-slice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        itemsList: [],
+        totalQuantity: 0,
+        showCart: false
+    }, 
+    reducers: {
+        addToCart(state, action) {
+            const newItem = action.payload; 
+            //to check if the item is already available 
+            const existingItem = state.itemsList.find((item)=> item.id === newItem.id);
+
+            if(existingItem) {
+                existingItem.totalQuantity++;
+                existingItem.price = newItem.price;
+            }else {
+                state.itemsList.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1, 
+                    totalPrice: newItem.price,
+                    name: newItem.name
+                })
+            }
+        },
+        removeFromCart() {}, 
+        setShowCart(state) {
+            state.showCart = true
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions; 
+
+export default cartSlice
