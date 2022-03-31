0934d49a0a947127cee08df42e83823283d98134
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-03-31 20:27:35 +0545

    added the layout of cart addition

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -16,7 +16,7 @@ const cartSlice = createSlice({
 
             if(existingItem) {
                 existingItem.totalQuantity++;
-                existingItem.price = newItem.price;
+                existingItem.totalPrice += newItem.price;
             }else {
                 state.itemsList.push({
                     id: newItem.id,
@@ -24,7 +24,8 @@ const cartSlice = createSlice({
                     quantity: 1, 
                     totalPrice: newItem.price,
                     name: newItem.name
-                })
+                });
+                    state.totalQuantity++;
             }
         },
         removeFromCart() {}, 
